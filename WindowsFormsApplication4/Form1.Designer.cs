146f32d01f20b67a2dc2c44e06095909225d65f0
Özer Aydın W <ozeraydin57@gmail.com>
2026-01-08 13:56:06 +0300

    sapb1 connection diapi

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.user = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.compDb = new System.Windows.Forms.TextBox();
+            this.sld = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.server = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lisans = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(295, 58);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(100, 20);
+            this.user.TabIndex = 0;
+            this.user.Text = "manager";
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(295, 93);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(100, 20);
+            this.pass.TabIndex = 1;
+            this.pass.Text = "*****";
+            // 
+            // compDb
+            // 
+            this.compDb.Location = new System.Drawing.Point(295, 134);
+            this.compDb.Name = "compDb";
+            this.compDb.Size = new System.Drawing.Size(100, 20);
+            this.compDb.TabIndex = 2;
+            this.compDb.Text = "SBODemoTR";
+            // 
+            // sld
+            // 
+            this.sld.Location = new System.Drawing.Point(295, 174);
+            this.sld.Name = "sld";
+            this.sld.Size = new System.Drawing.Size(100, 20);
+            this.sld.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // server
+            // 
+            this.server.Location = new System.Drawing.Point(295, 27);
+            this.server.Name = "server";
+            this.server.Size = new System.Drawing.Size(100, 20);
+            this.server.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "user";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "comp DB";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "sld";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(222, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "lisans";
+            // 
+            // lisans
+            // 
+            this.lisans.Location = new System.Drawing.Point(295, 216);
+            this.lisans.Name = "lisans";
+            this.lisans.Size = new System.Drawing.Size(100, 20);
+            this.lisans.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 394);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lisans);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.server);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sld);
+            this.Controls.Add(this.compDb);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.TextBox compDb;
+        private System.Windows.Forms.TextBox sld;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox server;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox lisans;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using SAPbobsCOM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Company company = new Company();
+
+            if (server.Text != "")
+                company.Server = server.Text;
+
+            company.UserName = user.Text;
+            company.Password = pass.Text;
+            company.CompanyDB = compDb.Text;
+            company.DbServerType = BoDataServerTypes.dst_MSSQL2019;
+
+            if (sld.Text != "")
+                company.SLDServer = sld.Text;
+
+            if (lisans.Text != "")
+                company.LicenseServer = lisans.Text;
+
+            var conn = company.Connect();
+
+            if (company.Connected)
+            {
+                MessageBox.Show(company.CompanyName);
+            }
+            else
+            {
+                MessageBox.Show(company.GetLastErrorCode() + " " + company.GetLastErrorDescription());
+            }
+        }
+    }
+}
